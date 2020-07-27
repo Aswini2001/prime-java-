7e543418db2f88a0eebe337147468a5e645950a9
Aswini2001 <64783778+Aswini2001@users.noreply.github.com>
2020-07-27 19:33:25 +0530

    created prime.java

---
+++ prime.java
@@ -0,0 +1,30 @@
+package myjava;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner (System.in);
+		System.out.println("enter the input number:\n");
+		int num = sc.nextInt();
+		boolean isPrime = true;
+		for (int i =2;i<num;i++) {
+			if (num%i==0) {
+				isPrime =false;
+				break;
+			}
+		}
+		if (isPrime)
+		{
+			System.out.println(num+" is a prime number");
+		}
+		else
+		{
+			System.out.println(num+" is not  a prime number");
+		}
+
+	}
+
+}
